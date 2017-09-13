6ccd7d3cdace6184d3bbe8d34b9c19b3dd3f8fc4
BENJAMIN LEROY WESLEY MORA <benjamin.wesley@ulatina.net>
2017-09-12 23:25:47 -0600

    Sep12

---
+++ Tarea1/Tarea1/Dominio/Tipo de Datos/Datos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea1.Dominio.Tipo_de_Datos
+{
+    class Datos
+    {
+        String Jugador1 = " ";
+        String Jugador2 = " ";
+        String Jugador3 = " ";
+
+    }
+}
